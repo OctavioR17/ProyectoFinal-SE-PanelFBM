70c33b693c00c7797cae4cbcfbca8d0a129e969d
carlosgd17 <carlosgd_17@outlook.com>
2024-06-24 23:47:40 -0600

    working fine

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@ const http = require('http');
 const { Client } = require('pg');
 require('dotenv').config();
 
-
 const hostname = '0.0.0.0';
 const port = 3000;
 
@@ -14,52 +13,61 @@ const client = new Client({
     port: process.env.DB_PORT,
 });
 
-//client.connect();
+client.connect();
 
-async function main(data) {
-    const fecha = new Date().toLocaleString('en-US', 
-        { 
-            year: 'numeric', 
-            month: '2-digit', 
-            day: '2-digit', 
-            hour: '2-digit', 
-            minute: '2-digit', 
-            second: '2-digit', 
-            hour12: false 
-        }
-    );
-    console.log(fecha);
+function getFecha() {
+    let fecha = new Date();
+    return fecha.getFullYear() + "/" +
+    (fecha.getMonth() + 1) + "/" +
+    fecha.getDate() + " " +
+    fecha.getHours() + ":" +
+    fecha.getMinutes() + ":" +
+    fecha.getSeconds()
+}
+
+async function insertar(data) {
+    const hora = getFecha();
+    
+    console.log(hora);
+    console.log(data);
     try{
-        console.log(data);
+        console.log("Insertando datos...");
 
-        const sesionid = await client.query(
-            'SELECT max(sesion.id) FROM sesion \
-                JOIN usuario ON usuarioid = usuario.id \
-                WHERE usuario.nombre LIKE $1', [data.nombre]
-                );
-        const sesion = sesionid.rows[0].max;
-
-        //const result = await client.query('SELECT * FROM test');
         const result = await client.query(
-            'INSERT INTO monitoreo (hora, intensidadled, oxigenacion, pulso, temperatura, sesionid) \
-                VALUES ($1, $2, $3, $4, $5, $6)', 
-                [fecha, data.intensidadled, data.oxigenacion, data.pulso, data.temperatura, sesion]
+            'INSERT INTO tratamiento (hora, tratamiento, intensidad, frecuencia, oxigenacion, pulso, temperatura) \
+                VALUES ($1, $2, $3, $4, $5, $6, $7)', 
+                [hora, data.tratamiento, data.intensidad, data.frecuencia, data.oxigenacion, data.pulso, data.temperatura]
         );
-
-        console.log(result.rows);
+        console.log("Datos insertados!");
     } catch (error) {
         console.error("Encountered an error: ", error);
     }
 }
 
+async function consultar(){
+    try {
+        const query = 'SELECT * FROM tratamiento'; // Replace with your actual query
+        const res = await client.query(query);
+        return res.rows; // Assuming you want to return the rows from the query
+      } catch (err) {
+        console.error(err);
+        throw err; // Or handle the error as needed
+      }
+}
 
 const server = http.createServer((req, res) => {
 
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Or '*' for any origin
-    res.setHeader('Access-Control-Allow-Methods', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Set CORS headers
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Change '*' to your frontend's origin for security
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    // Handle preflight requests
+    if (req.method === 'OPTIONS') {
+        res.statusCode = 204; // No Content
+        res.end();
+        return;
+    }
 
     console.log("\nRecibiendo peticion...");
     if (req.method === 'POST' && req.headers['content-type'] === 'application/json') {
@@ -70,19 +78,18 @@ const server = http.createServer((req, res) => {
         req.on('end', () => {
             const data = JSON.parse(body); // parse the JSON data
             res.end('JSON received and printed');
-            main(data);
+            insertar(data);
         });
     } else if (req.method === 'GET') {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({
-        id: 1,
-        date: new Date().toISOString(),
-        value1: 'Value 1',
-        value2: 'Value 2',
-        value3: 'Value 3'
-        }));
-        console.log("Data sent!");
+
+        consultar().then( result => {
+            const datos = result;
+
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(datos));
+            console.log("Data sent!");
+        })
     } else {
         res.statusCode = 404;
         res.end('Not Found');
